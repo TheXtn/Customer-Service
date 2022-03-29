d6f067a66664c331702ff10da3aad5d7741ed0ef
Islem Bargaoui <slash4games.yt@gmail.com>
2022-03-29 21:16:24 +0100

    Header Update

--- CustomerService/src/app/app.module.ts
+++ CustomerService/src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HelloComponent } from './Components/hello/hello.component';
-import { HiComponent } from './Components/hi/hi.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
-import { HeaderComponent } from './Components/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './component/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    HiComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
