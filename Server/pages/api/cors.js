f6ce946d08c4f4c988706dcf8eb083bb1f61dba5
Yussef Mrt <52961281+TheXtn@users.noreply.github.com>
2022-04-01 16:18:39 +0100

    UptoDate

--- Server/pages/api/cors.js
+++ Server/pages/api/cors.js
@@ -1,30 +1,30 @@
 import Cors from 'cors'
-function initMiddleware(middleware) {
-    return (req, res) =>
-      new Promise((resolve, reject) => {
-        middleware(req, res, (result) => {
-          if (result instanceof Error) {
-            return reject(result)
-          }
-          return resolve(result)
-        })
-      })
-  }
 
-// Initialize the cors middleware
-const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-    
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
   })
-)
+}
 
-export default async function handler(req, res) {
-  // Run cors
-  await cors(req, res)
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
 
   // Rest of the API logic
   res.json({ message: 'Hello Everyone!' })
 }
+
+export default handler
